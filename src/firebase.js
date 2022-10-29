b49412c2919dd934f9ee9218ad333f9efebbdfc2
wang-yurin <king_1994@naver.com>
2022-10-29 22:22:35 +0900

    Chore: firebase 초기 세팅

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+  apiKey: "AIzaSyDtgO7jAP90t3K3INcVOLL0_HKBHsHHwMU",
+  authDomain: "reactlove-b6b71.firebaseapp.com",
+  projectId: "reactlove-b6b71",
+  storageBucket: "reactlove-b6b71.appspot.com",
+  messagingSenderId: "714450304692",
+  appId: "1:714450304692:web:0c0984de87a373848221e1",
+};
+
+export default initializeApp(firebaseConfig);
