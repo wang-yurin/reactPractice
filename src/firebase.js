ad724483a47bc49d4fe694b3772a0a2b3f64b08a
wang-yurin <king_1994@naver.com>
2022-11-09 00:17:32 +0900

    Docs: firebase 셋팅 완료

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
 
-// eslint-disable-next-line no-unused-vars
 const firebaseConfig = {
-  apiKey: "AIzaSyDtgO7jAP90t3K3INcVOLL0_HKBHsHHwMU",
-  authDomain: "reactlove-b6b71.firebaseapp.com",
-  projectId: "reactlove-b6b71",
-  storageBucket: "reactlove-b6b71.appspot.com",
-  messagingSenderId: "714450304692",
-  appId: "1:714450304692:web:0c0984de87a373848221e1",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-export default initializeApp(firebaseConfig);
+export default firebase.initializeApp(firebaseConfig);
